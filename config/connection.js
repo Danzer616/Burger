ecb42b568768af55fbedfb3765757b25b61b41b0
Danzer <dan.alexander616@gmail.com>
2019-03-30 01:08:00 -0600

    updated sql and simplified js

--- config/connection.js
+++ config/connection.js
@@ -1,45 +1,22 @@
-// ==========================================
-// DEPENDENCIES
-// ==========================================
+// Set up MySQL connection.
 var mysql = require("mysql");
-// ==========================================
 
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "Cursed4312!",
+  database: "burgers_db"
+});
 
-// ==========================================
-// MAKE CONNECTION
-// ==========================================
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "Password",
-//     database: "burgers_db"
-//   });
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    // port: 3306,
-    user: "root",
-    password: "Cursed4312!",
-    database: "burgers_db"
-  });
-};
-
-
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("connected as id: " + connection.threadId);
 });
-// ==========================================
 
-// ==========================================
-// ORM EXPORT
-// ==========================================
+// Export connection for our ORM to use.
 module.exports = connection;
-// ==========================================
--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
 CREATE DATABASE burgers_db;
+
 USE burgers_db;
-CREATE TABLE burgers
-(
-    id INT NOT NULL
-    AUTO_INCREMENT,
-    burger_name VARCHAR
-    (100) NOT NULL,
-    devoured BOOLEAN NOT NULL DEFAULT FALSE,
-    PRIMARY KEY
-    (id)
+
+CREATE TABLE burgers (
+	id INT (10) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR (100) NOT NULL,
+    devoured BOOLEAN,
+    date TIMESTAMP,
+    PRIMARY KEY (id)
 );
