ed860df4fba651976469daa1abfc3402a00cbf6f
Danzer <dan.alexander616@gmail.com>
2019-04-16 05:21:00 -0600

    added server,orm,connection

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-
-var port = 3000;
+var exphbs = require("express-handlebars");
 
 var app = express();
+var PORT = process.env.PORT || 3000;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
-app.use("/", routes);
-
-app.listen(port);
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
